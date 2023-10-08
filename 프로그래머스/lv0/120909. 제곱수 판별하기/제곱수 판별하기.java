6dab185132b0dc7252aa05c63117ef9d016f8884
kyh0113 <71127157+kyh0113@users.noreply.github.com>
2023-10-09 00:08:01 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(n%(int)Math.sqrt(n)==0){
+            answer = 1;
+        }else{
+            answer = 2;
+        }
+        return answer;
+    }
+}
