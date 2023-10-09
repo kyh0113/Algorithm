4634483eed88dc5257443390973992b9196d6a9b
kyh0113 <71127157+kyh0113@users.noreply.github.com>
2023-10-09 22:40:24 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(long n) {
+        int answer = 0;
+        while(n>1){
+            if(n%2==0){
+                n= n/2;
+                answer++;
+            }else{
+                n= n*3+1;
+                answer++;
+            }
+        }
+        if(answer>=500){
+            answer = -1;
+        }
+        return answer;
+    }
+}
