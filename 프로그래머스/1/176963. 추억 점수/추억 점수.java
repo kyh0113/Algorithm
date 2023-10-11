dafa82d25cb77c5d8df4b662dadca871fb425464
kyh0113 <71127157+kyh0113@users.noreply.github.com>
2023-10-12 04:42:26 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+//         int[] answer = new int[photo.length];
+//         Map<String, Integer>map=new HashMap<>();
+//         for(int i =0; i< name.length; i++){
+//             map.put(name[i],yearning[i]);
+//         }
+
+//         for(int i =0; i< photo.length; i++){
+//             int sum = 0;
+//             for(int j =0; j<photo[i].length; j++){
+//                 if(map.containsKey(photo[i][j])){
+//                     sum+=map.get(photo[i][j]);
+//                     answer[i]= sum;
+//                 }
+//             }
+//         }
+//         // int[] answer = new int[photo.length];
+//         // for (int i = 0; i < photo.length; i++) {
+//         //     for (int j = 0; j < photo[i].length; j++) {
+//         //         for (int k = 0; k < name.length; k++) {
+//         //             if (photo[i][j].equals(name[k])) {
+//         //                 answer[i] += yearning[k];
+//         //             }
+//         //         }
+//         //     }
+//         // }
+        int[] answer = new int[photo.length];
+         for(int i =0; i< photo.length; i++){
+             int sum = 0;
+             for(int j =0; j<photo[i].length; j++){
+                 for(int k=0; k<name.length; k++){
+                     if(photo[i][j].equals(name[k])){
+                         sum+=yearning[k];
+                     }
+                 }
+             }
+             answer[i]=sum;
+         }
+        return answer;
+    }
+}
