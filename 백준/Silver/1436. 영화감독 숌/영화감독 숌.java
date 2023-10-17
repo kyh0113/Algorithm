14b9b722f314554a41e0f93259607e1f1acaae04
kyh0113 <71127157+kyh0113@users.noreply.github.com>
2023-10-17 18:56:06 +0900

    [Silver V] Title: 영화감독 숌, Time: 296 ms, Memory: 85948 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int num = 666;
+        int count = 1;
+        while(count !=N){
+            num++;
+            if(String.valueOf(num).contains("666")){
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
